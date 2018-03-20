9101b4bd7b03e9d0c80f38424ecea19c79969099
gky360 <gky360@gmail.com>
2018-03-20 14:57:31 +0900

    add get contest command

--- cmd/getContest.go
+++ cmd/getContest.go
@@ -25,18 +25,18 @@ import (
 	"github.com/spf13/viper"
 )
 
-type GetOptions struct {
+type GetContestOptions struct {
 	Out, ErrOut io.Writer
 }
 
-var getOpt = &GetOptions{
+var getContestOpt = &GetContestOptions{
 	Out:    os.Stdout,
 	ErrOut: os.Stderr,
 }
 
-// getCmd represents the get command
-var getCmd = &cobra.Command{
-	Use:   "get",
+// getContestCmd represents the getContest command
+var getContestCmd = &cobra.Command{
+	Use:   "contest",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -45,43 +45,30 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		if err := getOpt.Run(cmd, args); err != nil {
-			fmt.Fprintln(getOpt.ErrOut, err)
+		if err := getContestOpt.Run(cmd, args); err != nil {
+			fmt.Fprintln(getContestOpt.ErrOut, err)
 		}
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(getCmd)
+	getCmd.AddCommand(getContestCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// getCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// getContestCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// getCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// getContestCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
-func (opt *GetOptions) Run(cmd *cobra.Command, args []string) (err error) {
-	// get contest
+func (opt *GetContestOptions) Run(cmd *cobra.Command, args []string) (err error) {
 	contestID := viper.GetString("contest.id")
 	contest := new(models.Contest)
 	if _, err = Client.GetContest(contestID, contest); err != nil {
-		return err
-	}
-
-	// get tasks
-	var tasks []models.Task
-	if _, err = Client.GetTasks(contestID, &tasks); err != nil {
-		return err
-	}
-
-	// get submissions
-	var sbms []models.Submission
-	if _, err = Client.GetSubmissions(contestID, "", &sbms); err != nil {
 		return err
 	}
 
@@ -89,17 +76,8 @@ func (opt *GetOptions) Run(cmd *cobra.Command, args []string) (err error) {
 	if err != nil {
 		return err
 	}
-	tasksYaml, err := models.TasksToYaml(tasks)
-	if err != nil {
-		return err
-	}
-	sbmsYaml, err := models.SubmissionsToYamlShort(sbms)
-	if err != nil {
-		return err
-	}
+
 	fmt.Fprintln(opt.Out, contestYaml)
-	fmt.Fprintln(opt.Out, tasksYaml)
-	fmt.Fprintln(opt.Out, sbmsYaml)
 
 	return nil
 }
